c3bac7290dbd8aa6bfbdfe32ed1e0cd160cad9d1
Bruno Freitas <bruno_freitas@live.com>
2019-07-04 02:12:39 -0300

    remoção extends e ;

--- Namespace/class/Cadastro.php
+++ Namespace/class/Cadastro.php
@@ -1,49 +1,48 @@
 <?php 
 
-class Cadastro extends AnotherClass
+class Cadastro
 {
-	
-	private $name;
-	private $email;
-	private $pass;
+    
+    private $name;
+    private $email;
+    private $pass;
 
-	public function getName():string
-	{
-		return $this->name;
-	}
+    public function getName():string
+    {
+        return $this->name;
+    }
 
-	public function getEmail():string
-	{
-		return $this->email;
-	}
+    public function getEmail():string
+    {
+        return $this->email;
+    }
 
-	public function getPass():string
-	{
-		return $this->pass;
-	}
+    public function getPass():string
+    {
+        return $this->pass;
+    }
 
-	public function setName($name);
-	{
-		$this->name = $name;
-	}
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
 
-	public function setEmail($email);
-	{
-		$this->email = $email;
-	}
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
 
-	public function setPass($pass);
-	{
-		$this->pass = $pass;
-	}
+    public function setPass($pass)
+    {
+        $this->pass = $pass;
+    }
 
-	public function __toString()
-	{
-		return json_encode(array(
-			"nome"=>$this->getName(),
-			"email"=>$this->getEmail(),
-			"senha"=>$this->getPass()
-
-		));
-	}
+    public function __toString()
+    {
+        return json_encode(array(
+            "nome"=>$this->getName(),
+            "email"=>$this->getEmail(),
+            "senha"=>$this->getPass()
+        ));
+    }
 }
