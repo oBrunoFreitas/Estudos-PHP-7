b7818ab7cdf9cede81f507a41340b33d8932f70d
Bruno Freitas <bruno_freitas@live.com>
2019-07-04 01:48:06 -0300

    add classe Cadastro

---
+++ Namespace/class/Cadastro.php
@@ -0,0 +1,49 @@
+<?php 
+
+class Cadastro extends AnotherClass
+{
+	
+	private $name;
+	private $email;
+	private $pass;
+
+	public function getName():string
+	{
+		return $this->name;
+	}
+
+	public function getEmail():string
+	{
+		return $this->email;
+	}
+
+	public function getPass():string
+	{
+		return $this->pass;
+	}
+
+	public function setName($name);
+	{
+		$this->name = $name;
+	}
+
+	public function setEmail($email);
+	{
+		$this->email = $email;
+	}
+
+	public function setPass($pass);
+	{
+		$this->pass = $pass;
+	}
+
+	public function __toString()
+	{
+		return json_encode(array(
+			"nome"=>$this->getName(),
+			"email"=>$this->getEmail(),
+			"senha"=>$this->getPass()
+
+		));
+	}
+}
